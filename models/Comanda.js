db9eaaaa75ec64fe15562cddf5ead2e8c5fdfb22
unknown <22114290112@ACADEMICO.IESB.BR>
2024-04-03 21:59:44 -0300

    atualizando

--- models/Comanda.js
+++ models/Comanda.js
@@ -3,6 +3,14 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema({
     nome: String,
+    data:{
+        type: Date,
+        required: true,
+        set: (value) => {
+            const [dia, mes, ano] = value.split('/')
+            return `${mes}/${dia}/${ano}`
+        },
+    }
    
 })
 
--- models/Produto.js
+++ models/Produto.js
@@ -1,11 +1,30 @@
 const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema({
-    nome: String,
-    preco: Number,
-    tamanho: String,
-    tipo: String,
-    ingredientes: []
+    nome: {
+        type: String,
+        required: true,
+        minLength: 2,
+        maxLength: 100,
+        trim: true
+    },
+    preco:{
+       type: Number,
+       default: 0,
+       min: [0, 'Mais facil'],
+       match: /^\d{3}\.\d{3}\.\d{3}-\d{2}$/,
+ 
+    },
+    tamanho:{
+       type: String
+    },
+    tipo: {
+        type: String,
+        enum: ["Bebidas", "Pizzas", "Massas", "Sobremesas"]
+    },
+    ingredientes: [String]
+        
+   
 })
 
 const produto = mongoose.model('Produto', Schema)
